8f73f36c22866c335d74ff5b8ef05734a9a0e186
WojciechowskiRoman <roman.vaitsakhouski@gmail.com>
2021-05-17 18:45:58 +0200

    created 5 sections: header,banner, features, week-products and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
+var $jq = jQuery.noConflict();
+
 $(function() {
+
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    $('.banner__slider').slick({
+       arrows: false,
+       dots: true,
+       swipe: false,
+       adaptiveHeight: true,
+       autoplay: true,
+       autoplaySpeed: 3000
+    });
+
+    var mixer = mixitup('.week-products__cards');
 
 });
