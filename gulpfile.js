21bab714965af947e36695875713de532584ccb0
WojciechowskiRoman <roman.vaitsakhouski@gmail.com>
2021-06-10 11:36:24 +0200

    upgrated catalog aside filter and project structure

--- gulpfile.js
+++ gulpfile.js
@@ -89,7 +89,6 @@ function build() {
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/**/*.html']).on('change', browserSync.reload);
     watch(['app/images/**.svg'], svgSprite);
     watch(['app/html/**/*.html'], htmlInclude);
 }
